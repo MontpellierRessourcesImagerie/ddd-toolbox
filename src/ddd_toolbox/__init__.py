0e730bfec3613bcdfd16f2067cb3f8be26eb8022
volker-baecker <volker.baecker@mri.cnrs.fr>
2025-11-18 18:28:59 +0100

    started to setup the project

--- src/ddd_toolbox/__init__.py
+++ src/ddd_toolbox/__init__.py
@@ -7,10 +7,7 @@ except ImportError:
 from ._reader import napari_get_reader
 from ._sample_data import make_sample_data
 from ._widget import (
-    ExampleQWidget,
-    ImageThreshold,
-    threshold_autogenerate_widget,
-    threshold_magic_widget,
+    ConvolutionWidget,
 )
 from ._writer import write_multiple, write_single_image
 
@@ -19,8 +16,5 @@ __all__ = (
     "write_single_image",
     "write_multiple",
     "make_sample_data",
-    "ExampleQWidget",
-    "ImageThreshold",
-    "threshold_autogenerate_widget",
-    "threshold_magic_widget",
+    "ConvolutionWidget",
 )
